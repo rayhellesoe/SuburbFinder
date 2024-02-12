f8875efdfac8f065dec58ea660f2f3972879fcef
rayhellesoe <71107000+raybs360@users.noreply.github.com>
2024-02-12 20:08:54 +1300

    Create placeholder API endpoint to fetch suburb

---
+++ SuburbFinder.Server/Controllers/SuburbFinderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuburbFinder.Server.Models;
+
+namespace SuburbFinder.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class SuburbFinderController : ControllerBase
+    {
+        [HttpGet("{Id:int}")]
+        public Suburb Get(int Id)
+        {
+            Suburb suburb = new Suburb { Id = 1, Name = "Arch Hill", Latitude = -36.8659669, Longitude = 174.7148012 };
+            return suburb;
+        }
+    }
+}
---
+++ SuburbFinder.Server/Models/Suburb.cs
@@ -0,0 +1,10 @@
+﻿namespace SuburbFinder.Server.Models
+{
+    public class Suburb
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
