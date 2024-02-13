e8db5056d3f57331019c54114f1b27eca05b0857
rayhellesoe <71107000+raybs360@users.noreply.github.com>
2024-02-13 15:03:07 +1300

    Change order of properties for consistency

--- SuburbFinder.Server/Models/Coordinates.cs
+++ SuburbFinder.Server/Models/Coordinates.cs
@@ -2,7 +2,7 @@
 {
     public class Coordinates
     {
-        public double Longitude { get; set; }
         public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
--- SuburbFinder.Server/Models/Suburb.cs
+++ SuburbFinder.Server/Models/Suburb.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Longitude { get; set; }
         public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
