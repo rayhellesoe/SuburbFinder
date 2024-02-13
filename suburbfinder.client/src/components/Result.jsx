e21691c970a40ff5224a545b49e68e533acb1001
rayhellesoe <71107000+raybs360@users.noreply.github.com>
2024-02-13 20:23:05 +1300

    Style Result component

--- suburbfinder.client/src/components/Result.jsx
+++ suburbfinder.client/src/components/Result.jsx
@@ -1,12 +1,11 @@
 const Result = ({ suburb }) => {
     const { id, name, longitude, latitude } = suburb;
-    console.log(id, name, longitude, latitude)
 
     return (
-        <div>
-            <p>The nearest suburb is</p>
-            <h2>{name}</h2>
-            <p>Longitude: {longitude} - Latitude: {latitude}</p>
+        <div className="block max-w-md py-6 px-4 mt-12 bg-white border border-gray-200 rounded-lg shadow dark:bg-green-950 dark:border-green-900">
+            <p className="mb-4 text-md font-normal text-gray-700 dark:text-green-100">The nearest suburb is</p>
+            <h5 className="mb-4 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+            <p className="font-normal text-gray-700 dark:text-green-100"><span className="font-bold">Longitude:</span> {longitude} - <span className="font-bold">Latitude:</span> {latitude}</p>
         </div>
     )
 }
