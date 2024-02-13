988734662c56aad4f1a91476aab5183724a3d1c1
rayhellesoe <71107000+raybs360@users.noreply.github.com>
2024-02-13 20:42:33 +1300

    Update form input id and label

--- suburbfinder.client/src/components/CoordinatesForm.jsx
+++ suburbfinder.client/src/components/CoordinatesForm.jsx
@@ -23,8 +23,8 @@ const CoordinatesForm = ({ getNearestSuburb }) => {
                 <input type="number" id="latitude" onChange={updateInput} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="-100" required />
             </div>
             <div className="w-full md:w-1/2 inline-block p-2 text-left">
-                <label htmlFor="latitude" className="block mb-2 text-sm font-medium text-gray-900 dark:text-green-900">Latitude</label>
-                <input type="number" id="latitude" onChange={updateInput} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="-100" required />
+                <label htmlFor="longitude" className="block mb-2 text-sm font-medium text-gray-900 dark:text-green-900">Longitude</label>
+                <input type="number" id="longitude" onChange={updateInput} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="-100" required />
             </div>
             <div className="w-full inline-block px-2 text-left">
                 <button type="submit" className="bg-amber-500 hover:bg-amber-600 text-white p-2 mt-2 w-full cursor-pointer rounded-lg">
