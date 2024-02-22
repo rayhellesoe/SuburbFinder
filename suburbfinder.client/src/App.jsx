98c5beacdad70cb2ed9be8116e8fe2afbf1b5ccd
rayhellesoe <134174772+rayhellesoe@users.noreply.github.com>
2024-02-22 21:17:01 +1300

    conditionally render result component if nearestSuburb object is not empty

--- suburbfinder.client/src/App.jsx
+++ suburbfinder.client/src/App.jsx
@@ -5,7 +5,8 @@ import Result from './components/Result'
 
 const App = () => {
     const nearestSuburb = useSelector(state => state.suburbs)
-    const isNearestSuburbEmpty = Object.keys(nearestSuburb).length === 0;
+    const isNearestSuburbEmpty = Object.keys(nearestSuburb).length === 0
+    const resultElement = isNearestSuburbEmpty ? <></> : <Result suburb={nearestSuburb} />
 
     return (
         <main className="flex justify-center w-full">
@@ -13,7 +14,7 @@ const App = () => {
                 <h1 className="text-5xl font-bold mb-4">Suburb Finder</h1>
                 <p className="mb-6">Input a set of coordinates to find the nearest suburb.</p>
                 <CoordinatesForm />
-                {isNearestSuburbEmpty ? <></> : <Result suburb={nearestSuburb} />}
+                { resultElement }
             </div>
         </main>
     )
